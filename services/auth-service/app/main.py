c5815f164abab6c618cfdb0afcb6a6a52cb85821
JesusAndres2512 <estan5622@gmail.com>
2025-11-21 12:54:32 -0500

    fix: Correct audience claim in JWT token to support multiple endpoints

--- services/auth-service/app/main.py
+++ services/auth-service/app/main.py
@@ -86,7 +86,10 @@ def login(credentials: schemas.UserLogin, db: Session = Depends(get_db)):
             "sub": user.email,
             "role": user.role,
             "iss": "https://auth-service-apppettrack-caerbec2asefbwcd.canadacentral-01.azurewebsites.net/",
-            "aud": "https://api-gateway-apppettrack.azure-api.net/auth/"
+            "audi": [
+                "https://api-gateway-apppettrack.azure-api.net/auth/",
+                "https://api-gateway-apppettrack.azure-api.net/profile/"
+            ]
         },
         expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
