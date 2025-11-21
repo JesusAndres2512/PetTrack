d78bdca046e92a056f6c0696081fd59ed2696d2b
Parnol-06 <arnolferneyperez@hotmail.com>
2025-11-21 00:20:01 -0500

    refactor: Update authService.js to include /auth prefix for API routes

--- clients/web-client/src/api/authService.js
+++ clients/web-client/src/api/authService.js
@@ -1,8 +1,18 @@
 // src/services/authService.js
 import axios from "axios";
+
+// URL base del servicio de autenticación (Auth Service)
 const API_URL = import.meta.env.VITE_API_GATEWAY;
 
-export const login = async (data) => (await axios.post(`${API_URL}/login`, data)).data;
-export const register = async (data) => (await axios.post(`${API_URL}/register`, data)).data;
-export const getUsers = async () => (await axios.get(`${API_URL}/users`)).data; // agregar endpoint /users en el auth-service
+// Todas las rutas del Auth-Service deben comenzar con el prefijo /auth
+export const login = async (data) => (
+  await axios.post(`${API_URL}/auth/login`, data)
+).data;
 
+export const register = async (data) => (
+  await axios.post(`${API_URL}/auth/register`, data)
+).data;
+
+export const getUsers = async () => (
+  await axios.get(`${API_URL}/auth/users`)
+).data; 
